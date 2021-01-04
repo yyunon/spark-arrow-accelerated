7ea13c14506d69d7c71ba36d0191d52a784a56b6
yuksel <yonsel.yuksel@gmail.com>
2021-01-04 02:34:43 +0100

    Update example

--- spark-extension/src/test/scala/nl/tudelft/ewi/abs/nonnenmacher/fletcher/example/FletcherExampleSuite.scala
+++ spark-extension/src/test/scala/nl/tudelft/ewi/abs/nonnenmacher/fletcher/example/FletcherExampleSuite.scala
@@ -15,81 +15,22 @@ class FletcherExampleSuite extends FunSuite with SparkSessionGenerator {
 
   override def withExtensions: Seq[SparkSessionExtensions => Unit] = Seq(ArrowParquetReaderExtension, FletcherReductionExampleExtension)
 
-  ignore("convert taxi csv files to parquet file") {
-
-    val codec = "uncompressed"
-
-    val spark = SparkSession
-      .builder()
-      .appName(this.styleName)
-      .config("spark.master", "local")
-      .getOrCreate()
-
-    //write it as uncompressed file
-    spark.conf.set("spark.sql.parquet.compression.codec", codec)
-
-    val schema = new StructType()
-      .add(StructField("string", StringType, nullable = false))
-      .add(StructField("number", LongType, nullable = false))
-
-    val df = spark.read
-      .options(Map("header" -> "true")) //inferSchema is an expensive operation, but because it's just converted once we don't care
-      .schema(schema)
-      .csv("/Users/fabian/Downloads/taxi-2013.csv")
-      .repartition(1) //we want to have everything in 1 file
-      .limit(10000)
-
-    // When reading, Spark ignores the nullable property
-    // with this weird conversion we enforce it!
-    val df2 = df.sqlContext.createDataFrame(df.rdd, schema)
-
-    df2.write
-      .parquet(s"../data/taxi-$codec-10000")
-
-    spark.close()
-  }
-
-  ignore("parquet conversion") {
-
-    val codec = "uncompressed"
-
-    val spark = SparkSession
-      .builder()
-      .appName(this.styleName)
-      .config("spark.master", "local")
-      .getOrCreate()
-
-    //write it as uncompressed file
-    spark.conf.set("spark.sql.parquet.compression.codec", codec)
-
-    val schema = new StructType()
-      .add(StructField("string", StringType, nullable = false))
-      .add(StructField("number", LongType, nullable = false))
-
-    val df = spark.read
-      .parquet("/work/fnonnenmacher/data/chicago-taxi/taxi-uncompressed.parquet")
-      .repartition(1) //we want to have everything in 1 file
-      .limit(10 * 1000000)
-
-    // When reading, Spark ignores the nullable property
-    // with this weird conversion we enforce it!
-    val df2 = df.sqlContext.createDataFrame(df.rdd, schema)
-
-    df2.write
-      .parquet(s"../data/taxi-$codec")
-
-    spark.close()
-  }
-
   ignore("filter on regex and sum up int column") {
 
     //set batch size
     spark.conf.set("spark.sql.inMemoryColumnarStorage.batchSize", 500)
 
     val query =
-      """ SELECT SUM(`number`)
-        | FROM parquet.`../data/taxi-uncompressed-10000.parquet`
-        | WHERE `string` rlike 'Blue Ribbon Taxi Association Inc.' """.stripMargin
+      """ select
+          |    sum(l_extendedprice * l_discount) as revenue
+      from
+          |    /home/yyonsel/bulk/data/lineitem.parquet
+      where
+          |    l_shipdate >= date '1994-01-01'
+          |    and l_shipdate < date '1994-01-01' + interval '1' year
+          |    and l_discount between .06 - 0.01 and .06 + 0.01
+          |    and l_quantity < 24;
+      """.stripMargin
 
     val sqlDF = spark.sql(query)
 
@@ -106,9 +47,17 @@ class FletcherExampleSuite extends FunSuite with SparkSessionGenerator {
   }
 
   test("execution"){
-    val res = spark.sql(s""" SELECT SUM(`number`) as `number`
-                                   | FROM parquet.`../data/taxi-uncompressed.parquet`
-                                   | WHERE `string` rlike 'Blue Ribbon Taxi Association Inc.' """.stripMargin)
+    val query =
+      """ select
+          |    sum(l_extendedprice * l_discount) as revenue
+      from
+          |    /home/yyonsel/bulk/data/lineitem.parquet
+      where
+          |    l_shipdate >= date '1994-01-01'
+          |    and l_shipdate < date '1994-01-01' + interval '1' year
+          |    and l_discount between .06 - 0.01 and .06 + 0.01
+          |    and l_quantity < 24;
+      """.stripMargin
     println(res.collect())
   }
 }
